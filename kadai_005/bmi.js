55dc6aa6b01012e12497a433427a52f17fbd168a
pf93-mayakawa <kinoco.co3.may@gmail.com>
2025-03-11 21:24:45 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,18 @@
+// 変数の宣言・値の代入
+let bodyWeight = 68;
+let bodyHeight = 1.7;
+
+// コンソールへの出力
+console.log(bodyWeight);
+console.log(bodyHeight);
+
+// 四則演算
+console.log(bodyWeight / (bodyHeight * bodyHeight));
+
+// 変数の宣言・代入
+bodyWeight = 42;
+bodyHeight = 1.5;
+
+// コンソールへの出力
+console.log(bodyWeight);
+console.log(bodyHeight);
